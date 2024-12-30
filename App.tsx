cba647c2a37bffae5c22e59c8c9ddf0f5847753b
Cauã Campos <campos.eet@gmail.com>
2024-12-30 13:33:15 -0300

    Loading component

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
-import { StatusBar, View } from 'react-native';
+import { StatusBar } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
 import { GluestackUIProvider, Text, Center} from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
+import { Loading } from '@components/Loading';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular});
 
   return (
     <GluestackUIProvider config={config}>
-        { fontsLoaded ? 
-
-          <Center flex={1} bg='$gray700'> 
-            <Text>Home</Text> 
-          </Center> 
-
-        : <View /> }
-
         <StatusBar 
           barStyle="light-content"
           backgroundColor="transparent"
           translucent
         />
+
+        { fontsLoaded ? (
+
+          <Center flex={1} bg='$gray700'> 
+            <Text>Home</Text> 
+          </Center> 
+        ) : ( 
+          <Loading /> 
+        )}
+
+        
     </GluestackUIProvider>
   );
 }
