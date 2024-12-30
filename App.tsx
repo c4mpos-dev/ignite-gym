ac7b71ea5976a2dbff46dfebb09e067623fa945a
Cauã Campos <campos.eet@gmail.com>
2024-12-30 11:39:27 -0300

    Gluestack install

--- App.tsx
+++ App.tsx
@@ -1,18 +1,21 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/roboto';
+import { GluestackUIProvider, Text } from '@gluestack-ui/themed';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular});
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#202024"}}>
-      { fontsLoaded ? <Text>Home</Text> : <View /> }
+    <GluestackUIProvider>
+      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: "#202024"}}>
+        { fontsLoaded ? <Text>Home</Text> : <View /> }
 
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
-    </View>
+        <StatusBar 
+          barStyle="light-content"
+          backgroundColor="transparent"
+          translucent
+        />
+      </View>
+    </GluestackUIProvider>
   );
 }
