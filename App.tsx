be10863a2b0231e385584a27169015d8baf96c29
Cauã Campos <campos.eet@gmail.com>
2024-12-31 00:09:55 -0300

    BackgroundImg

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { useFonts, Roboto_700Bold, Roboto_400Regular } from '@expo-google-fonts/
 import { GluestackUIProvider, Text, Center} from '@gluestack-ui/themed';
 import { config } from './config/gluestack-ui.config';
 import { Loading } from '@components/Loading';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_700Bold, Roboto_400Regular});
@@ -16,10 +17,7 @@ export default function App() {
         />
 
         { fontsLoaded ? (
-
-          <Center flex={1} bg='$gray700'> 
-            <Text>Home</Text> 
-          </Center> 
+          <SignIn />
         ) : ( 
           <Loading /> 
         )}
