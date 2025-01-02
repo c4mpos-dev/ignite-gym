921bdae373824eb637ab53bde970b058617cd63a
Cauã Campos <campos.eet@gmail.com>
2025-01-02 15:31:56 -0300

    Group component

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
+import { HStack, VStack } from "@gluestack-ui/themed";
+
+import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
-import { VStack } from "@gluestack-ui/themed";
 
 export function Home(){
+    const [groupSelected, setGroupSelected] = useState("costa");
+
     return(
         <VStack flex={1}>
             <HomeHeader />
+            <HStack>
+                <Group 
+                    name="Costas" 
+                    isActive={groupSelected === "costa"} 
+                    onPress={() => setGroupSelected("costa")}
+                />
+
+                <Group 
+                    name="Ombro" 
+                    isActive={groupSelected === "ombro"} 
+                    onPress={() => setGroupSelected("ombro")}
+                />
+            </HStack>
         </VStack>
     );
 }
