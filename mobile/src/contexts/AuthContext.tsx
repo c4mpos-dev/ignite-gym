5d24558138fbd631ba81d833b4aabd4f6ef9e8f1
Cauã Campos <campos.eet@gmail.com>
2025-01-28 17:27:49 -0300

    Controller in SignIn

--- mobile/src/contexts/AuthContext.tsx
+++ mobile/src/contexts/AuthContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, ReactNode } from "react";
+import { createContext, ReactNode, useState } from "react";
 
 import { UserDTO } from "@dtos/UserDTO";
 
@@ -13,15 +13,15 @@ type AuthContextProviderProps = {
 export const AuthContext = createContext<AuthContextDataProps>({} as AuthContextDataProps);
 
 export function AuthContextProvider({ children }: AuthContextProviderProps){
+    const [user, setUser] = useState({
+        id: '1',
+        name: 'Cauã Campos',
+        email: 'Teste@gmail.com',
+        avatar: 'campos.png'
+    });
+
     return(
-        <AuthContext.Provider value={{
-            user: {
-                id: '1',
-                name: 'Cauã Campos',
-                email: 'Teste@gmail.com',
-                avatar: 'campos.png'
-            }
-        }}>
+        <AuthContext.Provider value={{ user }}>
             {children}
         </AuthContext.Provider>
     );
