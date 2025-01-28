ce56371f869de629055cd0124f80a547194473f5
Cauã Campos <campos.eet@gmail.com>
2025-01-27 23:30:29 -0300

    Create Context

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -5,6 +5,8 @@ import { config } from './config/gluestack-ui.config';
 
 import { Routes } from '@routes/index';
 
+import { AuthContext } from '@contexts/AuthContext';
+
 import { Loading } from '@components/Loading';
 
 export default function App() {
@@ -17,8 +19,14 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-
+      <AuthContext.Provider value={{
+        id: '1',
+        name: 'Caua',
+        email: 'Teste@gamail.com',
+        avatar: 'campos.png'
+      }}>
         { fontsLoaded ? <Routes/> : <Loading/> }
+      </AuthContext.Provider>
     </GluestackUIProvider>
   );
 }
--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useContext } from "react";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native"
 import { Box } from "@gluestack-ui/themed";
 import { gluestackUIConfig } from "../../config/gluestack-ui.config";
@@ -5,7 +6,13 @@ import { gluestackUIConfig } from "../../config/gluestack-ui.config";
 import { AuthRoutes } from "./auth.routes"
 import { AppRoutes } from "./app.routes";
 
+import { AuthContext } from "@contexts/AuthContext";
+
 export function Routes(){
+    const contextData = useContext(AuthContext);
+    console.log("USUARIO LOGADO =>", contextData);
+
+
     const theme = DefaultTheme;
     theme.colors.background = gluestackUIConfig.tokens.colors.gray700;
 
