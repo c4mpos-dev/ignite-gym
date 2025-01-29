50ebfc152b649f062cd5d2f1889e86b0a0ae3693
Cauã Campos <campos.eet@gmail.com>
2025-01-29 19:00:43 -0300

    Uploaded photo and username

--- mobile/src/components/HomeHeader.tsx
+++ mobile/src/components/HomeHeader.tsx
@@ -1,19 +1,25 @@
 import { Heading, HStack, Text, VStack, Icon } from "@gluestack-ui/themed";
-import { UserPhoto } from "@components/UserPhoto";
 import { LogOut } from "lucide-react-native";
 
+import { UserPhoto } from "@components/UserPhoto";
+import userPhotoDefault from "@assets/userPhotoDefault.png";
+
+import { useAuth } from "@hooks/useAuth";
+
 export function HomeHeader(){
+    const { user } = useAuth();
+
     return(
         <HStack bg="$gray600" pt="$16" pb="$5" px="$8" alignItems="center" gap="$4">
             <UserPhoto 
-                source={{ uri: "https://github.com/c4mpos-dev.png" }} 
+                source={ user.avatar ? { uri: user.avatar } : userPhotoDefault } 
                 alt="Foto do usuário"
                 w="$16"
                 h="$16"
             />
             <VStack flex={1}>
                 <Text color="$gray100" fontSize="$sm">Olá,</Text>
-                <Heading color="$gray100" fontSize="$md">Cauã Campos</Heading>
+                <Heading color="$gray100" fontSize="$md">{user.name}</Heading>
             </VStack>
 
             <Icon as={LogOut} color="$gray200" size="xl"/>
