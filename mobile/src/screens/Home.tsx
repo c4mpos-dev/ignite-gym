dd327c1b73a39f4285018a2faa207f054d84be2b
Cauã Campos <campos.eet@gmail.com>
2025-01-30 00:25:06 -0300

    Groups pulled from the backend

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,24 +1,55 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { FlatList } from "react-native";
-import { Heading, HStack, Text, VStack } from "@gluestack-ui/themed";
+import { useNavigation } from "@react-navigation/native";
+import { Heading, HStack, Text, VStack, useToast, Toast, ToastTitle, set } from "@gluestack-ui/themed";
 
+import { api } from "@services/api";
 import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { ExerciseCard } from "@components/ExerciseCard";
-import { useNavigation } from "@react-navigation/native";
+
+import { AppError } from "@utils/AppError";
 
 export function Home(){
     const [exercises, setExercises] = useState(["Puxada frontal", "Remada curvada", "Remada unilateral", "Levantamento terra", "Levantamento terra2", "Levantamento terra3", "Levantamento terra5", "Levantamento terra6"]);
-    const [groups, setGroups] = useState(["Costas", "Bíceps", " Tríceps", "Ombro"]);
+    const [groups, setGroups] = useState<string[]>([]);
     const [groupSelected, setGroupSelected] = useState("Costas");
 
+    const toast = useToast();
     const navigation = useNavigation<AppNavigatorRoutesProps>();
 
     function handleOpenExerciseDetails() {
         navigation.navigate("exercise");
     }
+
+    async function fetchGroups() {
+        try {
+            const response = await api.get("/groups");
+            setGroups(response.data);
+
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : "Erro ao buscar grupos musculares.";
+
+            if(!toast.isActive("error")) {
+                toast.show({
+                    id: "error",
+                    placement: "top",
+                    render: () => (
+                    <Toast backgroundColor='$red500' action="error" variant="outline" mt="$14">
+                        <ToastTitle  color="$white">{title}</ToastTitle>
+                    </Toast>
+                    ),
+                });
+            }
+        }
+    }
+
+    useEffect(() => {
+        fetchGroups();
+    }, []);
 
     return(
         <VStack flex={1}>
@@ -28,8 +59,8 @@ export function Home(){
                 keyExtractor={item => item}
                 renderItem={({ item }) => (
                     <Group 
-                        name={item}
-                        isActive={groupSelected.toLowerCase() === item.toLowerCase()} 
+                        name={item.toUpperCase()}
+                        isActive={groupSelected.toUpperCase() === item.toUpperCase()} 
                         onPress={() => setGroupSelected(item)}
                     />
                 )}
